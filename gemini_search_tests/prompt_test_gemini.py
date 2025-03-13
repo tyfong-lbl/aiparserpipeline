7624025627b2e335bd4440d5e84b1cd85eab7514
tyfong-lbl <tyfong@lbl.gov>
2025-03-13 15:40:19 -0700

    Add code to not ask url

--- gemini_search_tests/prompt_test_gemini.py
+++ gemini_search_tests/prompt_test_gemini.py
@@ -11,10 +11,10 @@ google_search_tool = Tool(google_search=GoogleSearch())
 
 test_prompt = """
                 You are an analyst that conducts solar energy project research.
-You have access to Google Search to look up news about solar energy projects from the last three months. You are trying to find out about NEW solar energy projects that have been annouced or started building. 
+You have access to Google Search to look up news about solar energy projects from the last two years. You are trying to find out about NEW solar energy projects that have been annouced or started building. 
 
-                 Question: Search the web for articles posted in the last three months about specific solar energy projects in the USA and return direct links to the articles, and NOT to pages that LINK to the articles. ONLY GET REAL EXISTING ARTICLES. The links MUST go to the SPECIFIC article. 
-                 Format: Return the data in a json format with the title of the article, url, and description as keys. 
+                 Question: Search the web for articles posted in the last two years about specific solar energy projects in the USA and return articles about projects. Return 10 projects.  
+                 Format: Return the data in a json format with the title of the article,  and description of the project as keys. 
 """
 
 response = client.models.generate_content(
